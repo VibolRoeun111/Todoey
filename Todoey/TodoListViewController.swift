05585c7b44761ad45f8751bcaf7853a43da21f92
Vibol's Macbook Pro <vibolsmacbookpro@vibolss-macbook-pro.local>
2018-04-01 13:38:09 +0700

    completed UserDefauls

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,10 +11,14 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["find", "Buy", "Destory"]
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items =  defaults.array(forKey: "TodoListArray") as? [String]{
+            itemArray = items
+        }
     }
 
   //MARK - Tableview Datasource Methods
@@ -49,6 +53,9 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the Add Item button on our UIAlert
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
